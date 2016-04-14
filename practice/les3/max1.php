b9cd218b4755bab09bd21a04aecc23e7267eb488
stako <folk89@rambler.ru>
2016-04-14 22:41:52 +0300

    2.28

---
+++ practice/les3/max1.php
@@ -0,0 +1,22 @@
+<html>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="MAX_FILE_SIZE" value="310000">
+        <input multiple="multiple" type="file" name="filename[]">
+        <br>
+        <input type="submit" name="submit" value="send">
+    </form>
+    <pre>
+        <?php
+        //01.25.00 max2
+        error_reporting(E_ALL);
+        if(isset($_POST['submit'])) {
+            print_r($_POST);
+            print_r($_FILES);
+            foreach ($_FILES['filename']['tmp_name'] as $k => $v) {
+                if (getimagesize($v)) {
+                    move_uploaded_file($v, "upload/" . $_FILES['filename']['name'][$k]);
+                }
+            }
+        }
+        ?>
+</html>
